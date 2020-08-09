893f21e57b2685e789b8dbc6ad1bddf43785513c
DavideC03 <davidecastellani@castellanidavide.it>
2020-08-09 21:28:53 +0200

    add send respose

--- server.js
+++ server.js
@@ -98,3 +98,21 @@ app.route('/users/:id')
     }
   )
 })
+function sendResponse(res,err,data){
+  if (err){
+    res.json({
+      success: false,
+      message: err
+    })
+  } else if (!data){
+    res.json({
+      success: false,
+      message: "Not Found"
+    })
+  } else {
+    res.json({
+      success: true,
+      data: data
+    })
+  }
+}
